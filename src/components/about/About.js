452696761ad96c8c15d88992d9bb3bb8666a7f3c
Keltuzad29 <c.cerquera.123@gmail.com>
2021-08-31 01:38:59 -0500

    cambio del about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className="about-container">
             <div className="about-desc">
                 <h3>Algo Acerca de Mi</h3>
-                <p>Yo soy Camilo Cerquera me gusta mucho la Programacion y aprender cada dia cosas nuevas, incursione en el desarrollo hace tiempo en un curso de sistemas y aprendiendo lo basico de Visual Basic, como todos haciendo mi primer Hola Mundo, despues me aleje un poco de este mundo y en la universidad estudiando Electronica y Telecomunicaciones vi un par de materias sobre el tema, siempre me emocione mucho cuando los programas funcionaban pero nunca crei que ser Desarrollador iba a ser mi oficio hasta que me enfrente al mercado laboral y no encajaba en ningun lado, luego conoci Henry me vi retado y acepte el desafio, ahora aqui estoy aprendiendo cada dia mas y especializandome en algo que en realidad me motiva y mueve fibras dentro de mi.</p>
+                <p>Yo soy Camilo Cerquera me gusta mucho la Programacion y aprender cada dia cosas nuevas, incursione en el desarrollo hace tiempo en un curso de sistemas y aprendiendo lo basico de Visual Basic, como todos haciendo mi primer Hola Mundo, despues me aleje un poco de esto y en la universidad estudiando Electronica y Telecomunicaciones vi un par de materias sobre el tema, siempre me emocione mucho cuando los programas funcionaban pero nunca crei que ser Desarrollador iba a ser mi oficio hasta que me enfrente al mercado laboral y no encajaba en ningun lado, luego conoci Henry me vi retado y acepte el desafio, ahora aqui estoy aprendiendo cada dia mas y especializandome en algo que en realidad me motiva y mueve fibras dentro de mi.</p>
             </div>
             <div className='about-img'>
                 <img src={foto} alt="about"/>
